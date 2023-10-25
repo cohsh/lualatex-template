894f6f0888a0e4d87d0f91c243aa065341444a4a
cohsh <82075327+cohsh@users.noreply.github.com>
2023-10-25 10:15:49 +0900

    :sparkles: Add load functions / to improve readability of template TeX files / by editted core.lua and new load.lua

--- utility/core.lua
+++ utility/core.lua
@@ -1,12 +1,18 @@
-function file_exists(path)
-    local file_hundle, error_str, error_code = io.open(path, "rb")
-    if file_hundle == nil then
-        return false
-    else
-        return true
-    end
-end
+package.preload['core'] = function()
+    local core = {}
 
-function command(command_name, command_value)
-    return "\\" .. command_name .. "{" .. command_value .. "}"
+    function core.file_exists(path)
+        local file_hundle, error_str, error_code = io.open(path, "rb")
+        if file_hundle == nil then
+            return false
+        else
+            return true
+        end
+    end
+
+    function core.command(command_name, command_value)
+        return "\\" .. command_name .. "{" .. command_value .. "}"
+    end
+
+    return core
 end
---
+++ utility/load.lua
@@ -0,0 +1,50 @@
+-- load.lua
+local load = {}
+local core = require("core")
+
+load.SubFile = {}
+load.SubFile.__index = load.SubFile
+
+function load.SubFile:new(sub, n_range_min, n_range_max)
+    local obj = {
+        n_range_min = n_range_min, n_range_max = n_range_max, sub = sub,
+        part="part", chapter="chapter", section="section"
+    }
+    setmetatable(obj, load.SubFile)
+    return obj
+end
+
+function load.SubFile:book()
+    for i = self.n_range_min, self.n_range_max do
+        for j = self.n_range_min, self.n_range_max do
+            for k = self.n_range_min, self.n_range_max do
+                local path = self.sub .. "/" .. self.part .. i .. "/" .. self.chapter .. j .. "/" .. self.section .. k .. ".tex"
+                if core.file_exists(path) then
+                    tex.sprint(core.command("subfile", path))
+                end
+            end
+        end
+    end
+end
+
+function load.SubFile:article()
+    for i = self.n_range_min, self.n_range_max do
+        for j = self.n_range_min, self.n_range_max do
+            local path = self.sub .. "/" .. self.part .. i .. "/" .. self.section .. j .. ".tex"
+            if core.file_exists(path) then
+                tex.sprint(core.command("subfile", path))
+            end
+        end
+    end
+end
+
+function load.SubFile:beamer()
+    for i = self.n_range_min, self.n_range_max do
+        local path = self.sub .. "/" .. self.section .. i .. ".tex"
+        if core.file_exists(path) then
+            tex.sprint(core.command("subfile", path))
+        end
+    end
+end
+
+return load
